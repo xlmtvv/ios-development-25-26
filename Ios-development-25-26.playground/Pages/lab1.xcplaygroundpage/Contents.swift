7063c13dc478d88d8c7b2fdec44047adc571fce6
Alisher Mutaliyev <mutaliyevv1@gmail.com>
2025-09-12 17:45:00 +0500

    initial commit. lab1

---
+++ Ios-development-25-26.playground/Pages/lab1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+let firstName = "Alisher"
+let lastName = "Mutaliyev"
+
+let age: Int = 20
+let birthYear = 2005
+
+let isStudent: Bool = true
+
+let height: Double = 1.77
+let weight: Double = 74
+let eyeColor: String = "brown"
+
+let currentYear: Int = 2025
+print("my age is \(currentYear - birthYear)")
+
+let hobby: String = "gym"
+let numberOfHobbies: Int = 3
+let favoriteNumber: Int = 21
+let isHobbyCreative: Bool = true
+
+let futureGoals = "to be happy 😊"
+
+let liveStory: String = """
+    My name is \(firstName) \(lastName). I am \(age) years old, born in \(birthYear). I am \(isStudent ? "a student" : "not a student"). My hobby is \(hobby). I have \(numberOfHobbies) in total, and my favorite number is \(favoriteNumber)! I am \(height) meters tall and weigh \(weight) kilograms. In the future, I want \(futureGoals).
+    """
+
+print(liveStory)
+
+
+
+
+ 
